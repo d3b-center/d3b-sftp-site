d83e1ff542ec5a8e44b5a61272f51cb8a74c4736
devbyaccident <blackdenc@chop.edu>
2024-05-13 13:03:49 -0400

    🚑 Fixing bad attribute ID for deployed resource

--- waf.tf
+++ waf.tf
@@ -1,5 +1,5 @@
 resource "aws_wafv2_web_acl_association" "example_acl_association" {
-  resource_arn = aws_api_gateway_stage.prod.invoke_arn
+  resource_arn = aws_api_gateway_stage.prod.arn
   web_acl_arn  = data.aws_wafv2_web_acl.web_acl_default.arn
 }
 
